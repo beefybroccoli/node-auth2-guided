3dbe95555888134e6e1d2295a6730ded6294fe61
Ladrillo <gabriel@cabrejas.net>
2021-11-09 18:24:02 +0100

    wip

--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 
 module.exports = function buildToken(user) {
   const payload = {
-    
+    subject: user.id,
+    username: user.username,
+    role: user.role,
   }
 }
