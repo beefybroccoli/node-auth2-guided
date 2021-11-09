cf57d9997a17a620924578d7e151e9b0a7f7df2b
Ladrillo <gabriel@cabrejas.net>
2021-11-09 19:12:22 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,4 +1,9 @@
+const { TOKEN_SECRET } = require('../../config')
+
+
 module.exports = (req, res, next) => {
   // pull token from a header Authorization
+  // check the token for validity
+  // (recreate the signature)
   next();
 };
