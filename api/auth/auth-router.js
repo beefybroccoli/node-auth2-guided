c0ad8cb1df72050f3859dc34c66058293b8f783f
Ladrillo <gabriel@cabrejas.net>
2021-11-09 18:20:11 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-
+const tokenBuilder = require('./token-builder');
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
@@ -28,6 +28,7 @@ router.post('/login', (req, res, next) => {
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         // HERE IS WHERE WE CREATE TOKEN AND APPEND IT TO THE RESPONSE
+        const token = tokenBuilder()
         res.status(200).json({
           message: `Welcome back ${user.username}!`,
         });
