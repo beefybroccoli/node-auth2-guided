2a58c5705efcf7ea1bdff4731a627794df471294
Ladrillo <gabriel@cabrejas.net>
2021-11-09 19:42:12 +0100

    wip

--- api/auth/check-role-middleware.js
+++ api/auth/check-role-middleware.js
@@ -1,13 +1,10 @@
-module.exports = role => (req, res,next ) =>{
-  
-}
-    if (req.decodedJwt.role !== role) {
-      next({
-        status: 403,
-        message: 'you have no power here!'
-      })
-    } else {
-      next()
-    }
+module.exports = role => (req, res, next) => {
+  if (req.decodedJwt.role !== role) {
+    next({
+      status: 403,
+      message: 'you have no power here!'
+    })
+  } else {
+    next()
   }
 }
