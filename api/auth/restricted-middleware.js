38bf8c69fd0a4a5eb3e378bf302fcb7f3f53064f
Ladrillo <gabriel@cabrejas.net>
2021-11-09 19:17:47 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -13,6 +13,7 @@ module.exports = (req, res, next) => {
   jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return next({
+        status: 401,m
         message: `bad tokenz: ${err.message}`
       })
     }
