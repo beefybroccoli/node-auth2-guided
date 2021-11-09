af0ae6da3035e4cfc47876c3f2c13c32fa119d41
Ladrillo <gabriel@cabrejas.net>
2021-11-09 19:39:32 +0100

    wip

--- api/auth/check-role-middleware.js
+++ api/auth/check-role-middleware.js
@@ -1,6 +1,6 @@
 module.exports = function (role) {
   return function(req, res, next) {
-    if (req.decodedJwt !== role) {
+    if (req.decodedJwt.role !== role) {
       next({
         status: 403,
         message: 'you have no power here!'
