996b36de442c42e4b87134fb340a181d07488036
Ladrillo <gabriel@cabrejas.net>
2021-11-09 19:34:40 +0100

    wip

--- api/auth/check-role-middleware.js
+++ api/auth/check-role-middleware.js
@@ -1,6 +1,10 @@
 module.exports = function (role) {
   return function(req, res, next) {
-    // we check that the role is good
-    next()
+    if (req.decodedJwt !== role) {
+      next({
+        status: 403,
+        
+      })
+    }
   }
 }
