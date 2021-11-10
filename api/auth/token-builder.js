fea321f4ac9f24402f47555e9ff1b8935ab121c8
fred <tedkimemail@yahoo.com>
2021-11-10 02:17:34 -0800

    ...

--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -1,7 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 function buildToken(user){
-    return 'faketoken';
+    // return `faketoken for user ${user.username}`;
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        role : user.role,
+    }
 }
 
 module.exports = {buildToken};
